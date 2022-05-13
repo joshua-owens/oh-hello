e88fe8e6afedd006025bd474e4eb4aca2270eab0
Josh Owens <me@jowens.dev>
2022-05-12 20:59:37 -0500

    eslint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import Header from './components/Header';
 import ChatHistory from './components/ChatHistory';
 import ChatInput from './components/ChatInput';
 import './App.css';
 
-import { useSocket } from './hooks';
+import {useSocket} from './hooks';
 
 export default function App() {
-   const [history, setHistory] = useState([]);
-   const socket = useSocket();
+	const [history, setHistory] = useState([]);
+	const socket = useSocket();
 
-   useEffect(() => {
-        socket.connect((msg) => {
-            console.log('New message');
-            setHistory(prev => {
-                return [...prev, msg];
-            });
-        });
-   }, [socket]);
+	useEffect(() => {
+		socket.connect(msg => {
+			console.log('New message');
+			setHistory(prev => [...prev, msg]);
+		});
+	}, [socket]);
 
-   return (
-       <div className='app'>
-           <Header />
-           <ChatHistory history={history} />
-           <ChatInput send={socket.send} />
-       </div>
-   );
+	return (
+		<div className="app">
+			<Header />
+			<ChatHistory history={history} />
+			<ChatInput send={socket.send} />
+		</div>
+	);
 }
--- frontend/src/hooks/index.js
+++ frontend/src/hooks/index.js
@@ -1,33 +1,34 @@
 const socket = new WebSocket('ws://localhost:9000/ws');
-    
+
 function connect(cb) {
-    console.log('Connecting');
+	console.log('Connecting');
 
-    socket.onopen = () => {
-        console.log('Successfully connected');
-    }
+	socket.onopen = () => {
+		console.log('Successfully connected');
+	};
 
-    socket.onmessage = (msg) => {
-        console.log('Message from websocket:', msg);
-    }
+	socket.onmessage = msg => {
+		console.log('Message from websocket:', msg);
+		cb();
+	};
 
-    socket.onclose = (event) => {
-        console.log('socket closed connection', event);
-    }
+	socket.onclose = event => {
+		console.log('socket closed connection', event);
+	};
 
-    socket.onerror = (error) => {
-        console.error('socket error', error);
-    }
+	socket.onerror = error => {
+		console.error('socket error', error);
+	};
 }
 
 function send(msg) {
-    console.log('sending msg', msg);
-    socket.send(msg);
+	console.log('sending msg', msg);
+	socket.send(msg);
 }
 
 export function useSocket() {
-    return {
-        connect,
-        send,
-    }
+	return {
+		connect,
+		send,
+	};
 }
