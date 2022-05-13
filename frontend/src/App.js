7ac93e9a7ddbcebd46529cd5d1da2be5803f81d0
Josh Owens <me@jowens.dev>
2022-05-12 19:48:42 -0500

    setting up websocket

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import Header from './components/Header';
+import ChatHistory from './components/ChatHistory';
+import ChatInput from './components/ChatInput';
+import './App.css';
+
+import { useSocket } from './hooks';
+
+export default function App() {
+   const [history, setHistory] = useState([]);
+   const socket = useSocket();
+
+   useEffect(() => {
+        socket.connect((msg) => {
+            console.log('New message');
+            setHistory(prev => {
+                return [...prev, msg];
+            });
+        });
+   }, [socket]);
+
+   return (
+       <div className='app'>
+           <Header />
+           <ChatHistory history={history} />
+           <ChatInput send={socket.send} />
+       </div>
+   );
+}
---
+++ frontend/src/hooks/index.js
@@ -0,0 +1,33 @@
+const socket = new WebSocket('ws://localhost:9000/ws');
+    
+function connect(cb) {
+    console.log('Connecting');
+
+    socket.onopen = () => {
+        console.log('Successfully connected');
+    }
+
+    socket.onmessage = (msg) => {
+        console.log('Message from websocket:', msg);
+    }
+
+    socket.onclose = (event) => {
+        console.log('socket closed connection', event);
+    }
+
+    socket.onerror = (error) => {
+        console.error('socket error', error);
+    }
+}
+
+function send(msg) {
+    console.log('sending msg', msg);
+    socket.send(msg);
+}
+
+export function useSocket() {
+    return {
+        connect,
+        send,
+    }
+}
