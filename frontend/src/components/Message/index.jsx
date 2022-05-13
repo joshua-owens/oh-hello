198f5b003e47a2d0911e15415c390d29b1b44af9
Josh Owens <me@jowens.dev>
2022-05-13 18:51:39 -0500

    message component

--- frontend/src/components/Message/index.jsx
+++ frontend/src/components/Message/index.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Message.scss';
 
-export default function Message() {
+export default function Message({ data }) {
+    const [message, setMessage] = useState(JSON.parse(data))
 
+    return (
+        <div className='message'>
+            {message.body}
+        </div>
+    );
 }
